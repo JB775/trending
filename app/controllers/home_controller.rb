df6008c5d14473963f05262f08008217dfded59a
John Bergandino <jbergandino@gmail.com>
2015-11-19 23:50:41 -0500

    added Twitter API, linked up ENV variables and started testing on front end

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	@trend1 = @client.trends(2471217).attrs.first[1][0][:name]
+  	@trend2 = @client.trends(2471217).attrs.first[1][1][:name]
+  	@trend3 = @client.trends(2471217).attrs.first[1][2][:name]
+  	@trend4 = @client.trends(2471217).attrs.first[1][3][:name]
+  	@trend5 = @client.trends(2471217).attrs.first[1][4][:name]
   end
 end
