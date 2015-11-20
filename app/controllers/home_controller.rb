a7834283bb4119ab5a07e7a0b57807a5114d8fdf
John Bergandino <jbergandino@gmail.com>
2015-11-20 15:00:13 -0500

    added temporary instagram implementation, google adsense

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,18 @@ require 'twitter'
 
 class HomeController < ApplicationController
   def index
+  	# Need to update this so we only need to make 1 request and just grab top 10 trends
   	@trend1 = Client.trends(2471217).attrs.first[1][0][:name]
   	@trend2 = Client.trends(2471217).attrs.first[1][1][:name]
   	@trend3 = Client.trends(2471217).attrs.first[1][2][:name]
-  	@trend4 = Client.trends(2471217).attrs.first[1][3][:name]
-  	@trend5 = Client.trends(2471217).attrs.first[1][4][:name]
+  	# @trend4 = Client.trends(2471217).attrs.first[1][3][:name]
+  	# @trend5 = Client.trends(2471217).attrs.first[1][4][:name]
+
+
+ #  	Add to View:
+ #  	<li><%= @trend2 %></li>
+	# <li><%= @trend3 %></li>
+	# <li><%= @trend4 %></li>
+	# <li><%= @trend5 %></li>
   end
 end
